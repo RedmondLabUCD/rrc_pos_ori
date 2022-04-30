dee9d641c209ff309fa52ea7cc41f9a4267b1db3
wq13552463699 <qiang.wang@ucdconnect.ie>
2022-04-30 23:27:56 +0100

    new teacher

--- model_test.py
+++ model_test.py
@@ -47,7 +47,7 @@ def main():
     step_size=50
     difficulty=4
     obs_type='default'
-    model_path = 'acmodel185.pt'
+    model_path = 'acmodel140.pt'
     #############
     
     # Make sim environment
